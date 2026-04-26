4b2d6d0bafca3f328222ad07bc5e44446e4a76ec
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-04-26 18:54:41 +0530

    streamframe

--- streamframe/test.py
+++ streamframe/test.py
@@ -6,6 +6,10 @@ df.append({"price": 100.0, "volume": 10.0})
 df.append({"price": 101.0, "volume": 12.0})
 df.append({"price": 102.0, "volume": 8.0})
 
+print("=====")
+
 print(df.mean("price"))
 print(df.variance("price"))
 print(df.last("price"))
+
+print("=====")
